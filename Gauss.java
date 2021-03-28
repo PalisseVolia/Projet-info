749f12485025fdb1d2e5d5a011cd160c2104fdec
Palisse Volia <palissev@gmail.com>
2021-03-28 19:01:13 +0200

    final dimanche barre + noeuds

--- Gauss.java
+++ Gauss.java
@@ -1,12 +1,3 @@
 public class Gauss
 {
-    public static void main(String[] args)
-    {
-
-        System.out.println("entrez le nombre d'equations");
-        int n = Lire.i();
-        Matrice mat = new Matrice(n);
-        mat.initialize();
-        mat.resolution();
-    }
 }
