dbf07a0e6c3fc53bb5422a368e5f287d470f4427
Palisse Volia <palissev@gmail.com>
2021-03-28 17:19:03 +0200

    noeud v1

--- Barre.java
+++ Barre.java
@@ -1,3 +1,6 @@
 public class Barre {
-
+    public static void main(String[] args) {
+        Noeud test = new Noeud();
+        System.out.println(test.identificationN + " coords: (" + test.abscisse + "," + test.ordonnée + ")");
+    }
 }
--- Noeud.java
+++ Noeud.java
@@ -1,17 +1,19 @@
-public class Noeud {
-    String identificationN;                 //nom noeud
+public class Noeud extends Barre {
     Double abscisse;                        
     Double ordonnée;
-    Double typeAppui;                          //simple (tranlation tangentielle), double (bloqué en translation), sans appui => peut etre leur donner un identifiant numérique(1,2,3)
+    int typeAppui;                                                       //simple (tranlation tangentielle), double (bloqué en translation), sans appui => peut etre leur donner un identifiant numérique(1,2,3)
+    String identificationN;                                                 //nom noeud
 
-    Noeud (Double abs, Double ord, Double appui) {
+    Noeud (Double abs, Double ord, String id, int appui) {
         this.abscisse = abs;
         this.ordonnée = ord;
+        this.identificationN = id;
         this.typeAppui = appui;
     }
-
-    public static void name() {
-        
+    Noeud () {
+        this.abscisse = 0.0;
+        this.ordonnée = 0.0;
+        this.identificationN = "Unidentified";
+        this.typeAppui = 0;
     }
-        
 }
