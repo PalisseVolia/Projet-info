3905df27656e9d1b89fb7945613dac64406cd2cd
mattrchrt8 <mattrchrt8@gmail.com>
2021-03-28 19:03:43 +0200

    m

--- Gauss.java
+++ Gauss.java
@@ -1,12 +1,15 @@
+
 public class Gauss
 {
-    public static void main(String[] args)
-    {
 
-        System.out.println("entrez le nombre d'equations");
-        int n = Lire.i();
-        Matrice mat = new Matrice(n);
-        mat.initialize();
-        mat.resolution();
-    }
+	public static void main(String[] args)
+	{
+
+		System.out.println("Entrez le nombre d'equations");
+		int n = Lire.i();
+		Matrice mat = new Matrice(n);
+		mat.initialize();
+		mat.resolution();
+	}
+
 }
