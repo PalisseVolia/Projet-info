52d04aee224169ab57b249b46e94d10f70881a8d
Palisse Volia <palissev@gmail.com>
2021-04-16 03:43:55 +0200

    Affichage 1.1

--- Interface/Fenetre.java
+++ Interface/Fenetre.java
@@ -8,12 +8,15 @@ public class Fenetre {
     public static void main(String[] args) {
         FenetreMain fenetrem = new FenetreMain();
         fenetrem.setVisible(true);
-        
     }
 }
 
 class FenetreMain extends JFrame implements ActionListener{
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;  
+    //private Dimension txt;
+    private JButton close, noeud;
+    private JMenuBar bar;
+    private JTextField noeuds;
 
     public FenetreMain() {
         setTitle("Treillis calculator 2000");
@@ -22,7 +25,6 @@ class FenetreMain extends JFrame implements ActionListener{
         bar = new JMenuBar();
         close = new JButton("Close");
         noeud = new JButton("noeud");
-        refresh = new JButton("refresh");
         noeuds = new JTextField();
         noeuds.setPreferredSize(new Dimension(300,200));
         noeuds.setEditable(false);
@@ -30,14 +32,10 @@ class FenetreMain extends JFrame implements ActionListener{
         contenu.setLayout(new FlowLayout());
         contenu.add(close);
         contenu.add(noeuds);
-        contenu.add(refresh);
         close.addActionListener(this);
         noeud.addActionListener(this);
-        refresh.addActionListener(this);
         setJMenuBar(bar);
         bar.add(noeud);
-
-        fen = new FenetreNoeud();
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -45,29 +43,26 @@ class FenetreMain extends JFrame implements ActionListener{
             dispose();
         }
         if (e.getSource() == noeud) {
+            FenetreNoeud fen = new FenetreNoeud(this);
             fen.setVisible(true);
-        }
-        if (e.getSource() == refresh) {
-            initNoeuds();
-        }
+            initNoeuds(fen.getNewajout());
+        }  
     }
 
-    public void initNoeuds() {
-        noeuds.setText(noeuds.getText().concat(fen.getNewajout()));
-        fen.setNewajout();
+    private void initNoeuds(String ajout) {
+        noeuds.setText(noeuds.getText().concat(ajout));
     }
-
-    //private Dimension txt;
-    private JButton close, noeud, refresh;
-    private JMenuBar bar;
-    private JTextField noeuds;
-    private FenetreNoeud fen;
 }
 
-class FenetreNoeud extends JFrame implements ActionListener{
+class FenetreNoeud extends JDialog implements ActionListener{
     private static final long serialVersionUID = 1L;
+    private JButton add;
+    private JTextField abscisse, ordonnee;
+    private Dimension txt;
+    private String newajout;
 
-    public FenetreNoeud(){
+    public FenetreNoeud(java.awt.Frame parent){
+        super(parent, true); //permet d'ouvrir le jDialog en modal
         setTitle("Ajout de noeud");
         setSize(500,300);
         txt = new Dimension(75,24);
@@ -86,25 +81,16 @@ class FenetreNoeud extends JFrame implements ActionListener{
         ordonnee.addActionListener(this);
     }
 
-    public String getNewajout(){
+    public String getNewajout() {
         return newajout;
     }
-    public void setNewajout(){
-        this.newajout = "";
-    }
+    
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == add) {
             String x = abscisse.getText();
             String y = ordonnee.getText();
-            newajout = x + "," + y;
-            abscisse.setText("");
-            ordonnee.setText("");
-            setVisible(false);
+            this.newajout = x + "," + y;
+            dispose();
         }
     }
-    
-    private JButton add;
-    private JTextField abscisse, ordonnee;
-    private Dimension txt;
-    private String newajout;
 }
