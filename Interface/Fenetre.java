9d86a50c46337ba2c750ea2c1ca16d6bbd3ba103
Palisse Volia <palissev@gmail.com>
2021-04-16 20:44:29 +0200

    Affichage 1.1.1

--- Interface/Fenetre.java
+++ Interface/Fenetre.java
@@ -12,36 +12,36 @@ public class Fenetre {
 }
 
 class FenetreMain extends JFrame implements ActionListener{
-    private static final long serialVersionUID = 1L;  
-    //private Dimension txt;
-    private JButton close, noeud;
-    private JMenuBar bar;
-    private JTextField noeuds;
+    private static final long serialVersionUID = 1L;
+
+    private JButton noeud;
+    private JTextArea noeuds;
+    private JLabel label1;
 
     public FenetreMain() {
         setTitle("Treillis calculator 2000");
         setSize(1500,800);
-        //txt = new Dimension(75,24);
-        bar = new JMenuBar();
-        close = new JButton("Close");
-        noeud = new JButton("noeud");
-        noeuds = new JTextField();
+        
+        JPanel pane1 = new JPanel();
+        noeuds = new JTextArea("");
         noeuds.setPreferredSize(new Dimension(300,200));
         noeuds.setEditable(false);
+        label1 = new JLabel("Noeuds");
+        pane1.add(noeuds);
+        pane1.add(label1);
+
         Container contenu = getContentPane();
         contenu.setLayout(new FlowLayout());
-        contenu.add(close);
-        contenu.add(noeuds);
-        close.addActionListener(this);
-        noeud.addActionListener(this);
+        contenu.add(pane1);
+        
+        JMenuBar bar = new JMenuBar();
         setJMenuBar(bar);
+        noeud = new JButton("noeud");
         bar.add(noeud);
+        noeud.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == close) {
-            dispose();
-        }
         if (e.getSource() == noeud) {
             FenetreNoeud fen = new FenetreNoeud(this);
             fen.setVisible(true);
@@ -54,31 +54,41 @@ class FenetreMain extends JFrame implements ActionListener{
     }
 }
 
-class FenetreNoeud extends JDialog implements ActionListener{
+class FenetreNoeud extends JDialog implements ActionListener{   //JDialog, comme JFrame mais peut etre executé en modal
     private static final long serialVersionUID = 1L;
-    private JButton add;
+
+    private JButton add, close;
     private JTextField abscisse, ordonnee;
     private Dimension txt;
-    private String newajout;
+    private String newajout = "";
+    private JTextArea noeuds;
 
     public FenetreNoeud(java.awt.Frame parent){
-        super(parent, true); //permet d'ouvrir le jDialog en modal
+        super(parent, true);                                    //permet d'ouvrir le jDialog en modal, ie pause la FenetreMain pdt l'execution de la FenetreNoeud
         setTitle("Ajout de noeud");
         setSize(500,300);
+        
         txt = new Dimension(75,24);
-        add = new JButton("Compute");
+        add = new JButton("ADD");
+        close = new JButton("Close");
         abscisse = new JTextField();
         ordonnee = new JTextField();
+        noeuds = new JTextArea("");
         abscisse.setPreferredSize(txt);
         ordonnee.setPreferredSize(txt);
+        noeuds.setPreferredSize(new Dimension(300,200));
+
         Container contenu = getContentPane();
         contenu.setLayout(new FlowLayout());
         contenu.add(add);
         contenu.add(abscisse);
         contenu.add(ordonnee);
+        contenu.add(close);
+        contenu.add(noeuds);
         add.addActionListener(this);
         abscisse.addActionListener(this);
         ordonnee.addActionListener(this);
+        close.addActionListener(this);
     }
 
     public String getNewajout() {
@@ -89,7 +99,12 @@ class FenetreNoeud extends JDialog implements ActionListener{
         if (e.getSource() == add) {
             String x = abscisse.getText();
             String y = ordonnee.getText();
-            this.newajout = x + "," + y;
+            newajout = newajout + x + "," + y + "\n";
+            abscisse.setText("");
+            ordonnee.setText("");
+            noeuds.setText(newajout);
+        }
+        if (e.getSource() == close) {
             dispose();
         }
     }
