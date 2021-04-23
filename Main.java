919a098ceb2f8ad7afa97585e19d2902c37e3220
Palisse Volia <palissev@gmail.com>
2021-04-23 20:15:08 +0200

    Noeuds 1.0.1

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        // récupérer des informations sur tous les noeuds depuis la liste/le nb de noeuds:
         // LNoeud lnoeud = new LNoeud();
-        // System.out.println(lnoeud.getListeNoeuds(0).abscisse);
+        // System.out.println(lnoeud.getListeNoeuds(2).getTypeSupport());
+        //System.out.println(lnoeud.getlisteNoeuds());
     }
 }
