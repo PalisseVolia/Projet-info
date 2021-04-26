806d3a20a6060438cfa8956c77e83398fa2325c3
Palisse Volia <palissev@gmail.com>
2021-04-26 17:50:18 +0200

    Affichage 1.1.5 barres

--- Interface/FenetreMain.java
+++ Interface/FenetreMain.java
@@ -10,9 +10,9 @@ import java.io.FileWriter;
 class FenetreMain extends JFrame implements ActionListener{
     private static final long serialVersionUID = 1L;
 
-    private JButton noeud, compute;
-    private JTextArea noeuds;
-    private JLabel label1;
+    private JButton noeud, barre, compute;
+    private JTextArea noeuds, barres;
+    private JLabel label1, label2;
 
     public FenetreMain() {
         setTitle("Treillis calculator 2000");
@@ -28,17 +28,31 @@ class FenetreMain extends JFrame implements ActionListener{
         pane1.add(noeuds, BorderLayout.SOUTH);
         pane1.add(label1, BorderLayout.NORTH);
 
+        JPanel pane2 = new JPanel();
+        pane2.setLayout(new BorderLayout());
+        barres = new JTextArea("");
+        barres.setPreferredSize(new Dimension(300,200));
+        barres.setEditable(false);
+        label2 = new JLabel("Barres :");
+        label2.setFont(new Font("Arial", Font.PLAIN, 20));
+        pane2.add(barres, BorderLayout.SOUTH);
+        pane2.add(label2, BorderLayout.NORTH);
+
         Container contenu = getContentPane();
         contenu.setLayout(new FlowLayout());
         contenu.add(pane1);
+        contenu.add(pane2);
         
         JMenuBar bar = new JMenuBar();
         setJMenuBar(bar);
         noeud = new JButton("noeud");
+        barre = new JButton("barre");
         compute = new JButton("compute");
         bar.add(noeud);
+        bar.add(barre);
         bar.add(compute);
         noeud.addActionListener(this);
+        barre.addActionListener(this);
         compute.addActionListener(this);
     }
 
@@ -47,13 +61,19 @@ class FenetreMain extends JFrame implements ActionListener{
             FenetreNoeud fen = new FenetreNoeud(this);
             fen.setVisible(true);
             initNoeuds(fen.getNewajout());
-        }  
+        }
+        if (e.getSource() == barre) {
+            FenetreBarre fen = new FenetreBarre(this);
+            fen.setVisible(true);
+            initBarres(fen.getNewajout());
+        }
         if (e.getSource() == compute) {
             try {
                 File dataf = new File("Data.txt");
                 dataf.delete();
                 BufferedWriter data = new BufferedWriter(new FileWriter("Data.txt",true));
-                data.write("NOEUDS" + "\n" + noeuds.getText() + "FINNOEUDS");
+                data.write("NOEUDS" + "\n" + noeuds.getText() + "FINNOEUDS" + "\n");
+                data.write("BARRES" + "\n" + barres.getText() + "FINBARRES" + "\n");
                 data.close();
             } catch (Exception err) {
                 System.out.println("Erreur :\n"+err);
@@ -62,6 +82,13 @@ class FenetreMain extends JFrame implements ActionListener{
     }
 
     private void initNoeuds(String ajout) {
+        ajout = ajout.replace("error : l'abscisse ou l'ordonnée n'est pas un réel"+"\n", "");
+        ajout = ajout.replace("error : veuillez entrer un identifiant de noeud"+"\n", "");
         noeuds.setText(noeuds.getText().concat(ajout));
+    }
+    private void initBarres(String ajout) {
+        ajout = ajout.replace("error : deux noeuds similaires sélectionnés" +"\n", "");
+        ajout = ajout.replace("error : veuillez entrer un identifiant de barre" +"\n", "");
+        barres.setText(barres.getText().concat(ajout));
     }
 }
