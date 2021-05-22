563368c5eeabdc0f97821be3064c40d6e44669c6
eotter01 <emma.otter@insa-strasbourg.fr>
2021-05-22 13:27:11 +0200

    gaussf

--- Gauss.java
+++ Gauss.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
+
+import Treillis.LNoeud;
+import Treillis.Noeud;
+import Treillis.LBarre;
+import Terrain.LTriangle;
+import Terrain.Triangle;
+
 import java.lang.*;
 import java.util.*;
 
 public class Gauss {
-	ArrayList<ArrayList<Double>> M;
-	
+	public ArrayList<ArrayList<Double>> M;
+	public LBarre lBarre = new LBarre();
+	public LTriangle lTriangle = new LTriangle();
+
 	public Gauss(){
 		M = new ArrayList<ArrayList<Double>>();
 	}
@@ -23,11 +32,7 @@ public class Gauss {
 		M=res;
 		return res;
 	}
-	//MATRICE DE MEMES TAILLES !
-	/*       entrée               sortie
-	 * (a1 a2)    (b1 b2)      (a1 a2 b1 b2)
-	 * (a3 a4)    (b3 b4)      (a3 a4 b3 b4)
-	 */
+
 	
 	public ArrayList<ArrayList<Double>> echLigne(int i,int j){
 		ArrayList<Double> temp = M.get(i);
@@ -65,8 +70,9 @@ public class Gauss {
 		return M;
 	}
 	
-	public ArrayList<ArrayList<Double>> pGauss(ArrayList<ArrayList<Double>> A, ArrayList<ArrayList<Double>> B) {
-		M = augmente(A,B);//On crée la matrice augmentée de M
+	public ArrayList<ArrayList<Double>> pGauss(ArrayList<ArrayList<Double>> A) {
+		//M = augmente(A,B);//On crée la matrice augmentée de M
+		this.M = A;
 		int n = M.size();
 		for(int i=0; i<n-1; i++) {//On parcours chaque ligne de la matrice M
 			int p = pivot(i);//On cherche le plus grand pivot
@@ -86,24 +92,190 @@ public class Gauss {
 		return M;
 	}
 	
-	
-	
-	
-	public static void main(String[] args) {
-		Gauss test = new Gauss();
+	public static ArrayList<ArrayList<Double>> remplitmatrice(LNoeud list, LBarre lBarre) {
+        ArrayList<String> col = new ArrayList<String>();
+		ArrayList<ArrayList<Double>> Mat = new ArrayList<ArrayList<Double>>();
+        for(int i=0; i<length(list.getlisteNoeuds()); i++){
+            Noeud n = list.getlisteNoeuds()[i];
+        	for(int j=0; j<n.getTabbar().size(); j++){
+        		if(!col.contains(n.getTabbar().get(j))){
+            		col.add(n.getTabbar().get(j));
+       			}
+      	  }
+            if(n.getTypeSupport == 1){
+				col.add("R"+ n.getIdentificationN());
+            }
+            if(n.getTypeSupport == 2){
+				col.add("Rx"+ n.getIdentificationN());
+				col.add("Ry"+ n.getIdentificationN());
+            }
+        }
+		for(int i=0; i<length(list.getlisteNoeuds()); i++){
+			Noeud n = list.getlisteNoeuds()[i];
+			ArrayList<Double> Ligne1 = new ArrayList<Double>();
+			ArrayList<Double> Ligne2 = new ArrayList<Double>();
+			for(int a = 0; a<col.size(); a++){
+				Ligne1.add(0.0);
+				Ligne2.add(0.0);
+				}
+			if(n.getTypeSupport == 1){
+				for(int j=0; j<n.getTabbar().size(); j++){
+					for(int a = 0; a<col.size(); a++){
+						if (n.getTabbar().get(j) == col.get(a)){
+							double x1  = n.getabscisse();
+							double y1 = n.getordonnee();
+
+							for(int k = 0;  k<lBarre.getlistebarre().size();k++{
+								if(lBarre.getlisteBarre().get(k).getIdentificationB()==n.getTabbar(j)){
+									if(n.getIdentificationN()==lBarre.getlisteBarre().get(k).getDebut_noeud().getIdentificationN(){
+										double x2 = lBarre.getlisteBarre().get(k).getFin_noeud().getabscisse();
+										double y2 = lBarre.getlisteBarre().get(k).getFin_noeud().getordonnee();
+									}
+									else{
+										double x2 = lBarre.getlistBbarre().get(k).getDebut_noeud().getabscisse();
+										double y2 = lBarre.getlisteBarre().get(k).getDebut_noeud().getordonnee();
+									}
+								}
+							}
+							Ligne1.set(a,Math.cos(trouve_angle(x2,x1,1000.0,y2,y1,y1)));
+							Ligne2.set(a,Math.sin(trouve_angle(x2,x1,1000.0,y2,y1,y1)));
+						}
+						if (("R" + n.getIdentificationN()) == col.get(a)){
+							Triangle t = lTriangle.getlisteTriangle(n.gettriangleappui());
+							if (n.gettrianglecote() == 1)){
+								double x1 = t.getAbscisseT1();
+								double y1 = t.getOrdonneeT1();
+								double x2 = t.getAbscisseT2();
+								double y2 = t.getOrdonneeT2();
+								Ligne1.set(a, Math.cos(trouve_beta(x1, x2, y1, y2)));
+								Ligne2.set(a, Math.sin(trouve_beta(x1, x2, y1, y2)));
+							}
+							if (n.gettrianglecote() == 2)){
+								double x1 = t.getAbscisseT2();
+								double y1 = t.getOrdonneeT2();
+								double x2 = t.getAbscisseT3();
+								double y2 = t.getOrdonneeT3();
+								Ligne1.set(a, Math.cos(trouve_beta(x1, x2, y1, y2)));
+								Ligne2.set(a, Math.sin(trouve_beta(x1, x2, y1, y2)));
+							}
+							if (n.gettrianglecote() == 3)){
+								double x1 = t.getAbscisseT3();
+								double y1 = t.getOrdonneeT3();
+								double x2 = t.getAbscisseT1();
+								double y2 = t.getOrdonneeT1();
+								Ligne1.set(a, Math.cos(trouve_beta(x1, x2, y1, y2)));
+								Ligne2.set(a, Math.sin(trouve_beta(x1, x2, y1, y2)));	
+							}
+						}
+					}					
+
+				}
+			}
+			if(n.getTypeSupport == 2){
+				for(int j=0; j<n.getTabbar().size(); j++){
+					for(int a = 0; a<col.size(); a++){
+						if (n.getTabbar().get(j) == col.get(a)){
+							double x1  = n.getabscisse();
+							double y1 = n.getordonnee();
+
+							for(int k = 0;  k<lBarre.getlistebarre().size();k++{
+								if(lBarre.getlisteBarre().get(k).getIdentificationB()==n.getTabbar(j)){
+									if(n.getIdentificationN()==lBarre.getlisteBarre().get(k).getDebut_noeud().getIdentificationN(){
+										double x2 = lBarre.getlisteBarre().get(k).getFin_noeud().getabscisse();
+										double y2 = lBarre.getlisteBarre().get(k).getFin_noeud().getordonnee();
+									}
+									else{
+										double x2 = lBarre.getlistBbarre().get(k).getDebut_noeud().getabscisse();
+										double y2 = lBarre.getlisteBarre().get(k).getDebut_noeud().getordonnee();
+									}
+								}
+							}
+							Ligne1.set(a,Math.cos(trouve_angle(x2,x1,1000.0,y2,y1,y1)));
+							Ligne2.set(a,Math.sin(trouve_angle(x2,x1,1000.0,y2,y1,y1)));
+						}
+						if (("RX" + n.getIdentificationN()) == col.get(a)){
+							Ligne1.set(a,1.0);
+						}
+						if (("RY" + n.getIdentificationN()) == col.get(a)){
+							Ligne2.set(a,1.0);
+						}
+
+					}					
+
+				}
+			}
+			if(n.getTypeSupport == 3){
+				for(int j=0; j<n.getTabbar().size(); j++){
+					for(int a = 0; a<col.size(); a++){
+						if (n.getTabbar().get(j) == col.get(a)){
+							double x1  = n.getabscisse();
+							double y1 = n.getordonnee();
+
+							for(int k = 0;  k<lBarre.getlistebarre().size();k++{
+								if(lBarre.getlisteBarre().get(k).getIdentificationB()==n.getTabbar(j)){
+									if(n.getIdentificationN()==lBarre.getlisteBarre().get(k).getDebut_noeud().getIdentificationN(){
+										double x2 = lBarre.getlisteBarre().get(k).getFin_noeud().getabscisse();
+										double y2 = lBarre.getlisteBarre().get(k).getFin_noeud().getordonnee();
+									}
+									else{
+										double x2 = lBarre.getlistBbarre().get(k).getDebut_noeud().getabscisse();
+										double y2 = lBarre.getlisteBarre().get(k).getDebut_noeud().getordonnee();
+									}
+								}
+							}
+							Ligne1.set(a,Math.cos(trouve_angle(x2,x1,1000.0,y2,y1,y1)));
+							Ligne2.set(a,Math.sin(trouve_angle(x2,x1,1000.0,y2,y1,y1)));
+						}
+
+
+					}					
+
+				}
+				
+
+			}
+			Mat.add(Ligne1);
+			Mat.add(Ligne2);
+		}
+		return Mat;
+
+	}
+
+	public static double trouve_beta(double PTx1,double PTx2, double PTy1, double PTy2) {
+        double beta = (trouve_angle(PTx1,PTx2,1000,PTy1,PTy2,PTy2) + Math.PI/2)% Math.PI;
+        return beta;
+    }
+
+	public static double[] trouve_alpha(double x1,double x2, double x3, double y1, double y2, double y3) {
+        double[] res = new double[6];
+
+        double alpha12 = trouve_angle(x2,x1,1000,y2,y1,y1);
+        res[0]=alpha12;
+
+        double alpha13 = trouve_angle(x3,x1,1000,y3,y1,y1);
+        res[1]=alpha13;
+
+        double alpha21 = Math.PI-alpha12;
+        res[2]=alpha21;
+
+        double alpha23 = trouve_angle(x3,x2,1000,y3,y2,y2);
+        res[3]=alpha23;
+
+        double alpha31 = Math.PI-alpha13;
+        res[4]=alpha31;
+
+        double alpha32 = Math.PI-alpha23;
+        res[5]=alpha32;
+
+        return res;
+    }
+	public static void gaussfin(){
+		Gauss res = new Gauss();
 		
-		ArrayList<ArrayList<Double>> A = new ArrayList<ArrayList<Double>>();
-		//à rentrer
+		ArrayList<ArrayList<Double>> A = remplitmatrice(lBarre.lnoeud, lBarre);
 		
-		ArrayList<ArrayList<Double>> B = new ArrayList<ArrayList<Double>>();
-		//à rentrer
-		
-		
-		
-		
-		ArrayList<ArrayList<Double>> result = test.pGauss(A,B);
+		ArrayList<ArrayList<Double>> result = res.pGauss(A);
 		//résultat à récupérer
 		System.out.println(result);//pour le voir
 	}
-
 }
